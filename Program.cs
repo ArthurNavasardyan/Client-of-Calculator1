9bc3e1891e6251b213b9d241e87617300bbf24aa
ArthurNavasardyan <arthur.navasardyan5@gmail.com>
2023-09-14 20:33:46 +0400

    Client

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSignalR
+{
+    internal class Program
+    {
+        static HubConnection hubConnection;
+
+        static async Task Main(string[] args)
+        {
+            hubConnection = new HubConnectionBuilder()
+                 .WithUrl("https://localhost:7087/hub")
+                 .Build();
+
+            hubConnection.On<double>("Send", result => Console.WriteLine($"message from server:{result}"));
+
+            await hubConnection.StartAsync();
+
+            bool isExit = false;
+
+            while (!isExit)
+            {
+                var massage = Console.ReadLine();
+
+                if (massage == "exit")
+                {
+                    isExit = true;
+                }
+                
+            }
+            Console.ReadLine();
+
+        }
+
+
+
+    }
+}
